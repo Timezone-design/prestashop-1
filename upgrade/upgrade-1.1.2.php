9637593b4a3aea57d97f16d6752d5e1ee4c98d27
Thijs Simonis <thijs.simonis@gettonline.com>
2021-08-19 10:18:06 +0200

    fix: table name typo

--- upgrade/upgrade-1.1.2.php
+++ upgrade/upgrade-1.1.2.php
@@ -4,7 +4,7 @@ function upgrade_module_1_1_2(Module $module)
 {
     foreach(Db::getInstance()->executeS('
         SELECT `carrier`.* FROM `ps_carrier` AS `carrier`
-	    LEFT JOIN `ps_myparcebe_carrier_configuration` AS `config` ON `carrier`.`id_carrier` = `config`.`id_carrier` AND `config`.`name` = "carrierType"
+	    LEFT JOIN `ps_myparcelbe_carrier_configuration` AS `config` ON `carrier`.`id_carrier` = `config`.`id_carrier` AND `config`.`name` = "carrierType"
 	    WHERE `carrier`.`external_module_name` = "' . $module::MODULE_NAME . '"
 	    AND `config`.`id_configuration` IS NULL;
     ') as $record) {
